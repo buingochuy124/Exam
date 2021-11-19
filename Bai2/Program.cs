e90971f21f8cfeca30d656dfa54507d372cd2f07
buingochuy124 <buingochuy124@gmail.com>
2021-11-19 21:39:00 +0700

    CommitBai2

---
+++ Bai2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Bai2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = "gcdmember";
+            string password = "gcd123";
+            Console.Write("Enter username:   ");
+            string CheckTK = Console.ReadLine();
+            Console.Write("Enter username:   ");
+            string CheckMK = Console.ReadLine();
+            if (CheckTK == userName)
+            {
+                if (CheckMK == password)
+                {
+                    Console.WriteLine("Successfully !!!");
+                }
+                else
+                {
+                    Console.WriteLine("FAIL !!!");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
