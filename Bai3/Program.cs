eb7de71d963218bb02306a439e1a5831ec484533
buingochuy124 <buingochuy124@gmail.com>
2021-11-19 21:40:51 +0700

    CommitBai3

---
+++ Bai3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Bai3
+{
+    class Program
+    {
+        static void Menu()
+        {
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1. Option 1");
+            Console.WriteLine("2. Option 2");
+            Console.WriteLine("0. Exit");
+            Console.WriteLine();
+            Console.Write("Select an option:   ");
+        }
+        static void Main(string[] args)
+        {
+            int option;
+            do
+            {
+                Menu();
+                option = Convert.ToInt32(Console.ReadLine());
+            }
+            while (option != 0);
+
+            Console.ReadLine();
+        }
+    }
+}
